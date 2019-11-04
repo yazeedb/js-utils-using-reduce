f2acbfb1df141245e1cef85a903ab04bd5bad4c3
yazeedb <ybzadough@gmail.com>
2019-11-04 08:20:47 -0500

    Almost done. Don't accidentally delete this time...

--- functions.js
+++ functions.js
@@ -1,12 +1,58 @@
-const pipe = () => {};
-const compose = () => {};
-const zip = () => {};
-const intersperse = () => {};
-const insert = () => {};
-const flatten = () => {};
-const flatMap = () => {};
-const includes = () => {};
+const pipe = (...functions) => (initialValue) =>
+  functions.reduce((value, fn) => fn(value), initialValue);
+
+const compose = (...functions) => (initialValue) =>
+  functions.reduceRight((value, fn) => fn(value), initialValue);
+
+const zip = (list1, list2) => {
+  const sourceList = list1.length > list2.length ? list2 : list1;
+
+  return sourceList.reduce((acc, _, index) => {
+    const value1 = list1[index];
+    const value2 = list2[index];
+
+    acc.push([value1, value2]);
+
+    return acc;
+  }, []);
+};
+
+const intersperse = (separator, list) =>
+  list.reduce((acc, value, index) => {
+    if (index === list.length - 1) {
+      acc.push(value);
+    } else {
+      acc.push(value, separator);
+    }
+
+    return acc;
+  }, []);
+
+const insert = (index, newItem, list) => {
+  if (index > list.length - 1) {
+    return [...list, newItem];
+  }
+
+  return list.reduce((acc, value, sourceArrayIndex) => {
+    if (index === sourceArrayIndex) {
+      acc.push(newItem, value);
+    } else {
+      acc.push(value);
+    }
+
+    return acc;
+  }, []);
+};
+
+const flatten = (list) => list.reduce((acc, value) => acc.concat(value), []);
+
+const flatMap = (mappingFunction, list) => flatten(list.map(mappingFunction));
+
+const includes = (item, list) =>
+  list.reduce((isIncluded, value) => isIncluded || item === value, false);
+
 const compact = () => {};
+
 const arrayIntoObject = () => {};
 
 exports.pipe = pipe;
--- test.js
+++ test.js
@@ -11,13 +11,114 @@ const {
   arrayIntoObject
 } = require('./functions');
 
-test('pipe', () => {});
-test('compose', () => {});
-test('zip', () => {});
-test('intersperse', () => {});
-test('insert', () => {});
-test('flatten', () => {});
-test('flatMap', () => {});
-test('includes', () => {});
+test('pipe combines functions from left-to-right', () => {
+  const mathSequence = pipe(
+    (x) => x * 2,
+    (x) => x - 1,
+    (x) => x * 3
+  );
+
+  const outputs = [1, 2, 3].map(mathSequence);
+
+  expect(outputs).toEqual([3, 9, 15]);
+});
+
+test('compose combines functions from right-to-left', () => {
+  const mathSequence = compose(
+    (x) => x * 2,
+    (x) => x - 1,
+    (x) => x * 3
+  );
+
+  const outputs = [1, 2, 3].map(mathSequence);
+
+  expect(outputs).toEqual([4, 10, 16]);
+});
+
+test("zip pairs items from two lists, using the shorter list's length", () => {
+  const outputs = [
+    zip([1, 3], [2, 4]),
+    zip([1, 3, 5], [2, 4]),
+    zip([1, 3], [2, 4, 5]),
+    zip(['Decode', 'secret'], ['this', 'message!'])
+  ];
+
+  expect(outputs).toEqual([
+    [[1, 2], [3, 4]],
+    [[1, 2], [3, 4]],
+    [[1, 2], [3, 4]],
+    [['Decode', 'this'], ['secret', 'message!']]
+  ]);
+});
+
+test('intersperse injects a separator between each element of a list', () => {
+  const outputs = [
+    intersperse('Batman', [1, 2, 3, 4, 5, 6]),
+    intersperse('Batman', [])
+  ];
+
+  expect(outputs).toEqual([
+    [1, 'Batman', 2, 'Batman', 3, 'Batman', 4, 'Batman', 5, 'Batman', 6],
+    []
+  ]);
+});
+
+test('insert injects an element at the given index, injecting at the end if the index is too large', () => {
+  const outputs = [
+    insert(0, 'Batman', [1, 2, 3]),
+    insert(1, 'Batman', [1, 2, 3]),
+    insert(2, ['Batman'], [1, 2, 3]),
+    insert(10, ['Batman'], [1, 2, 3])
+  ];
+
+  expect(outputs).toEqual([
+    ['Batman', 1, 2, 3],
+    [1, 'Batman', 2, 3],
+    [1, 2, ['Batman'], 3],
+    [1, 2, 3, ['Batman']]
+  ]);
+});
+
+test('flatten will flatten an array by one level', () => {
+  const outputs = [
+    [[1, 2], [3, 4]],
+    [[1, 2], [[3, 4]]],
+    [[[1, 2]], [3, 4]],
+    [[[1, 2], [3, 4]]]
+  ].map(flatten);
+
+  expect(outputs).toEqual([
+    [1, 2, 3, 4],
+    [1, 2, [3, 4]],
+    [[1, 2], 3, 4],
+    [[1, 2], [3, 4]]
+  ]);
+});
+
+test('flatMap maps each element, then flattens the result', () => {
+  const outputs = [
+    flatMap((n) => [n * 2], [1, 2, 3, 4]),
+    flatMap((n) => [n, n], [1, 2, 3, 4]),
+    flatMap((s) => s.split(' '), ['flatMap', 'should be', 'mapFlat'])
+  ];
+
+  expect(outputs).toEqual([
+    [2, 4, 6, 8],
+    [1, 1, 2, 2, 3, 3, 4, 4],
+    ['flatMap', 'should', 'be', 'mapFlat']
+  ]);
+});
+
+test('includes tells if a list has a certain element', () => {
+  const outputs = [
+    includes(3, [1, 2, 3]),
+    includes(3, [1, 2]),
+    includes(0, [])
+  ];
+
+  expect(outputs).toEqual([true, false, false]);
+});
+
 test('compact', () => {});
+
 test('arrayIntoObject', () => {});
